7b93d535c0fab6d8a1cd9124299c99f8be129ab8
rstrawbe <kon.razinkin@yandex.ru>
2020-09-07 22:04:20 +0300

    add parser map

--- main.c
+++ main.c
@@ -16,47 +16,90 @@
 //    return (0);
 //}
 
+//static int validate_first_line(char *line)
+//{
+//
+//}
+
 static int validate_first_line(char *line)
 {
-
+    while (*line)
+        if (*line++ != '1' || *line != '\t')
+            return (0);
+    return (1);
 }
 
-static void create_map(const char *filename)
+static int create_map(const char *filename)
 {
 
     int fd;
     char	*line;
     int     i;
+    char    *line_clean;
 
     i = 0;
     line = NULL;
-    fd = open(filename, O_RDONLY);
+    line_clean = NULL;
 
-    while(get_next_line(fd, &line) == 1)
+    if ((fd = open(filename, O_RDONLY)) == -1)
     {
-        if (++i == 1)
-            validate_first_line(line);
-        printf("%s\n", line);
-        free(line);
+        perror("open failed on input file");
+        return (0);
     }
-    if (i <= 2)
-        printf("lines is <= 2");
+    while(get_next_line(fd, &line) >= 1)
+    {
+        if (!(line_clean = ft_strtrim(line, "\t")))
+        {
+            perror("err create line");
+            return (1);
+        }
 
-    validate_first_line(line);
+        printf("%s\n", line_clean);
 
-    printf("%s\n", line);
+        if (++i == 1 && !validate_first_line(line_clean)) {
+            perror("Map is not valid");
+            return (1);
+        }
+
+        free(line);
+        free(line_clean);
+    }
+    if (i <= 2) {
+        perror("lines is <= 2");
+        return (0);
+    }
+
+    if (!validate_first_line(line_clean))
+    {
+        perror("Map is not valid");
+        return (0);
+    }
+
+
+    printf("%s\n", line_clean);
     free(line);
+    //free(line_clean);
+
+    return (1);
 
 
 }
 
 
-int             main(void)
+int             main(int argc, char **argv)
 {
 //    void    *mlx;
 //    void    *mlx_win;
 
-    create_map("map.cub");
+    if (argc < 2) {
+        perror("Error. You must add path/to/map.cub as second argument!");
+        return (1);
+    }
+
+
+    if (!create_map( argv[1])) {
+        perror("Error create_map");
+    }
 
 //    printf("S isAllowed: %d\n", is_allowed_char('S'));
 //    printf("1 isAllowed: %d\n", is_allowed_char('1'));
