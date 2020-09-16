dc9fdbf4c7afe1b4a6e5553fbeb310665236cad4
rstrawbe <kon.razinkin@yandex.ru>
2020-09-16 20:33:31 +0300

    fill method

--- cube3d.h
+++ cube3d.h
@@ -1,5 +1,34 @@
 #ifndef CUB3D_H
-#define CUB3D_H
+# define CUB3D_H
 
-#define ALLOWED_MAP_CHARS "012NSEW"
+# define ALLOWED_MAP_CHARS " 012NSEW"
+# define ALLOWED_DIRECTIONS "NSEW"
+
+
+typedef struct s_hero {
+    int     pos_x_start;
+    int     pos_y_start;
+
+    int     pos_x;
+    int     pos_y;
+
+    char    direction;
+    int     vector_x;
+    int     vector_y;
+}               t_hero;
+
+typedef struct  s_map
+{
+    char        *north_texture;
+    char        *south_texture;
+    char        *west_texture;
+    char        *east_texture;
+    char        *sprite_path;
+    int         floor_color;
+    int         cell_color;
+    int         width;
+    int         height;
+    char        vector_start;
+    char        **field;
+}               t_map;
 #endif
--- main.c
+++ main.c
@@ -5,80 +5,459 @@
 
 #include "get_next_line/get_next_line.h"
 
-//static int is_allowed_char(char ch)
-//{
-//    char    *allowed_str;
-//
-//    allowed_str = &ALLOWED_MAP_CHARS[0];
-//    while (*allowed_str)
-//        if (*allowed_str++ == ch)
-//            return (1);
-//    return (0);
-//}
+static t_map map;
 
-//static int validate_first_line(char *line)
-//{
-//
-//}
+static t_hero hero;
 
-static int validate_first_line(char *line)
+static int is_line_of_allowed_char(char *line)
 {
-    while (*line)
-        if (*line++ != '1' || *line != '\t')
+    char    *allowed_str;
+
+    allowed_str = &ALLOWED_MAP_CHARS[0];
+    while (*line) {
+        if (!(ft_strchr(allowed_str, *line++)))
             return (0);
+    }
     return (1);
 }
 
-static int create_map(const char *filename)
+static  void map_print(char **arr)
 {
-
-    int fd;
-    char	*line;
-    int     i;
-    char    *line_clean;
+    int i;
+    int j;
 
     i = 0;
+    j = 0;
+    while (i < map.height)
+    {
+        while (j < map.width)
+            ft_putchar_fd(arr[i][j++], 1);
+        ft_putchar_fd('\n', 1);
+        i++;
+        j = 0;
+    }
+}
+
+static  char **map_copy()
+{
+    int i;
+    int j;
+    char **copy_map;
+
+    i = 0;
+    j = 0;
+
+    if (!(copy_map = (char **)malloc(sizeof(char **) * map.height + 1)))
+        return (NULL);
+
+    while (i < map.height) {
+        if (!(copy_map[i++] = (char *) malloc(sizeof(char) * map.width + 1)))
+        {
+            while (--i >= 0)
+                free(copy_map[i]);
+            free(copy_map);
+            return (NULL);
+        }
+    }
+    *copy_map[i - 1] = '\0';
+
+    i = 0;
+    j = 0;
+
+    while (copy_map[i])
+        copy_map[i++][map.width] = '\0';
+    i = 0;
+    while (i < map.height)
+    {
+        while (j < map.width)
+        {
+            copy_map[i][j] = map.field[i][j];
+            j++;
+        }
+        i++;
+        j = 0;
+    }
+    return (copy_map);
+}
+
+static void append_map_line(char *line, int i)
+{
+    int j;
+    char s;
+
+    j = 0;
+    while (line[j])
+    {
+        s = (line[j] == ' ') ? '1' : line[j];
+        map.field[i][j++] = s;
+
+    }
+    while (j < map.width)
+        map.field[i][j++] = '1';
+}
+
+static void turn_right(t_hero *h)
+{
+    if (h->direction == 'N')
+    {
+        h->vector_x = 1;
+        h->vector_y = 0;
+        h->direction = 'E';
+    } else if (h->direction == 'W')
+    {
+        h->vector_x = 0;
+        h->vector_y = -1;
+        h->direction = 'N';
+    } else if (h->direction == 'S')
+    {
+        h->vector_x = -1;
+        h->vector_y = 0;
+        h->direction = 'W';
+    } else if (h->direction == 'E')
+    {
+        h->vector_x = 0;
+        h->vector_y = 1;
+        h->direction = 'S';
+    }
+
+}
+
+static void turn_left(t_hero *h)
+{
+    if (h->direction == 'N')
+    {
+        h->vector_x = -1;
+        h->vector_y = 0;
+        h->direction = 'W';
+    } else if (h->direction == 'W')
+    {
+        h->vector_x = 0;
+        h->vector_y = 1;
+        h->direction = 'S';
+    } else if (h->direction == 'S')
+    {
+        h->vector_x = 1;
+        h->vector_y = 0;
+        h->direction = 'E';
+    } else if (h->direction == 'E')
+    {
+        h->vector_x = 0;
+        h->vector_y = -1;
+        h->direction = 'N';
+    }
+
+}
+
+static char next_val(t_hero *h, char **arr)
+{
+    if (h->direction == 'N')
+    {
+        if (h->pos_y - 1 < 0)
+            return ('e');
+        return (arr[h->pos_y - 1][h->pos_x]);
+    } else if (h->direction == 'W')
+    {
+        if (h->pos_x - 1 < 0)
+            return ('e');
+        return (arr[h->pos_y][h->pos_x - 1]);
+    } else if (h->direction == 'S')
+    {
+        if (h->pos_y + 1 >= map.height)
+            return ('e');
+        return (arr[h->pos_y + 1][h->pos_x]);
+    } else if (h->direction == 'E')
+    {
+        if (h->pos_x + 1 >= map.width)
+            return ('e');
+        return (arr[h->pos_y][h->pos_x + 1]);
+    }
+    return ('e');
+}
+
+static char right_val(t_hero *h, char **arr)
+{
+    if (h->direction == 'N')
+        return (arr[h->pos_y][h->pos_x + 1]);
+    else if (h->direction == 'W')
+        return (arr[h->pos_y - 1][h->pos_x]);
+    else if (h->direction == 'S')
+        return (arr[h->pos_y][h->pos_x - 1]);
+    else if (h->direction == 'E')
+        return (arr[h->pos_y + 1][h->pos_x]);
+    return ('e');
+}
+
+static void set_direction(t_hero *h, char c)
+{
+    if (c == 'N')
+    {
+        h->vector_x = 0;
+        h->vector_y = -1;
+    }
+    if (c == 'W')
+    {
+        h->vector_x = -1;
+        h->vector_y = 0;
+    }
+    if (c == 'S')
+    {
+        h->vector_x = 0;
+        h->vector_y = 1;
+    }
+    if (c == 'E')
+    {
+        h->vector_x = 1;
+        h->vector_y = 0;
+    }
+    h->direction = c;
+}
+
+static void step_forward(t_hero *h)
+{
+    h->pos_x += h->vector_x;
+    h->pos_y += h->vector_y;
+}
+
+
+static int create_map(const char *filename)
+{
+    int i;
+    int j;
+    int     fd;
+    char	*line;
+    int     str_len;
+    char    **copy_map;
+    t_hero  copy_hero;
+    int     check_process;
+
     line = NULL;
-    line_clean = NULL;
+
+    map.width = 0;
+    map.height = 0;
+
+    i = 0;
+    j = 0;
 
     if ((fd = open(filename, O_RDONLY)) == -1)
-    {
-        perror("open failed on input file");
         return (0);
-    }
-    while(get_next_line(fd, &line) >= 1)
+    while(get_next_line(fd, &line) >= 0 && (str_len = ft_strlen(line)) && ++map.height)
     {
-        if (!(line_clean = ft_strtrim(line, "\t")))
+        if (!(is_line_of_allowed_char(line))) {
+            printf("no valid!\n");
+            return (0);
+        }
+
+        i = 0;
+        while (line[i])
         {
-            perror("err create line");
-            return (1);
+            if (line[i] == 'N')
+            {
+                if (hero.pos_x_start)
+                    return (0);
+                hero.pos_x_start = i;
+                hero.pos_y_start = map.height - 1;
+                set_direction(&hero, line[i]);
+            }
+
+            if (line[i] == 'W')
+            {
+                if (hero.pos_x_start)
+                    return (0);
+
+                hero.pos_x_start = i;
+                hero.pos_y_start = map.height - 1;
+                set_direction(&hero, line[i]);
+            }
+
+            if (line[i] == 'S')
+            {
+                if (hero.pos_x_start)
+                    return (0);
+
+                hero.pos_x_start = i;
+                hero.pos_y_start = map.height - 1;
+                set_direction(&hero, line[i]);
+            }
+
+            if (line[i] == 'E')
+            {
+                if (hero.pos_x_start)
+                    return (0);
+
+                hero.pos_x_start = i;
+                hero.pos_y_start = map.height - 1;
+                set_direction(&hero, line[i]);
+            }
+            i++;
         }
 
-        printf("%s\n", line_clean);
-
-        if (++i == 1 && !validate_first_line(line_clean)) {
-            perror("Map is not valid");
-            return (1);
-        }
-
+        map.width = str_len > map.width ? str_len : map.width;
         free(line);
-        free(line_clean);
     }
-    if (i <= 2) {
-        perror("lines is <= 2");
+
+    if (map.height <= 2) {
+        printf("lines is <= 2");
         return (0);
     }
-
-    if (!validate_first_line(line_clean))
-    {
-        perror("Map is not valid");
-        return (0);
-    }
-
-
-    printf("%s\n", line_clean);
     free(line);
-    //free(line_clean);
+
+    close(fd);
+    if ((fd = open(filename, O_RDONLY)) == -1)
+        return (0);
+
+    map.field = (char **)malloc(sizeof(char **) * map.height + 1);
+
+    i = 0;
+    j = 0;
+
+    while (i < map.height)
+        map.field[i++] = (char *)malloc(sizeof(char) * map.width + 1);
+
+    *map.field[i - 1] = '\0';
+
+    i = 0;
+    j = 0;
+
+    while (map.field[i])
+        map.field[i++][map.width] = '\0';
+
+    i = 0;
+    j = 0;
+    while (get_next_line(fd, &line) >= 1)
+    {
+        append_map_line(line, i++);
+        free(line);
+    }
+    append_map_line(line, i++);
+    free(line);
+
+
+//    printf("pos_x_start: %d\n", hero.pos_x_start);
+//    printf("pos_y_start: %d\n", hero.pos_y_start);
+//    printf("vector_x: %d\n", hero.vector_x);
+//    printf("vector_y: %d\n\n\n", hero.vector_y);
+
+    if (!(copy_map = map_copy()))
+        return (0);
+
+
+    copy_hero.vector_x = hero.vector_x;
+    copy_hero.vector_y = hero.vector_y;
+
+    copy_hero.pos_x_start = hero.pos_x_start;
+    copy_hero.pos_y_start = hero.pos_y_start;
+
+    copy_hero.pos_x = hero.pos_x_start;
+    copy_hero.pos_y = hero.pos_y_start;
+
+    copy_hero.direction = hero.direction;
+
+    check_process = 1;
+
+    char s;
+    int cnt = 0;
+    while (cnt < 4)
+    {
+        copy_hero.pos_y = hero.pos_y_start;
+        copy_hero.pos_x = hero.pos_x_start;
+        set_direction(&copy_hero, ALLOWED_DIRECTIONS[cnt++]);
+
+        while (next_val(&copy_hero, copy_map) == '0'
+        || next_val(&copy_hero, copy_map) == ' ') {
+            step_forward(&copy_hero);
+        }
+        copy_hero.pos_y_start = copy_hero.pos_y;
+        copy_hero.pos_x_start = copy_hero.pos_x;
+        turn_left(&copy_hero);
+
+        while (next_val(&copy_hero, copy_map) != 'e') {
+
+            s = ' ';
+            copy_map[copy_hero.pos_y][copy_hero.pos_x] = s;
+
+            system("clear");
+            map_print(copy_map);
+
+            printf("\n\npos_x_start: %d\n", copy_hero.pos_x_start);
+            printf("pos_y_start: %d\n", copy_hero.pos_y_start);
+
+            printf("pos_x: %d\n", copy_hero.pos_x);
+            printf("pos_y: %d\n", copy_hero.pos_y);
+            printf("direction: %c\n", copy_hero.direction);
+            printf("vector_x: %d\n", copy_hero.vector_x);
+            printf("vector_y: %d\n", copy_hero.vector_y);
+
+            printf("nval: %c\n", next_val(&copy_hero, copy_map));
+            printf("rval: %c\n", right_val(&copy_hero, copy_map));
+
+            usleep(100000);
+
+            if (next_val(&copy_hero, copy_map) == '1'
+            && right_val(&copy_hero, copy_map) != '0'
+            && right_val(&copy_hero, copy_map) != ' ') {
+                turn_left(&copy_hero);
+            } else if ((right_val(&copy_hero, copy_map) == '0'
+                || right_val(&copy_hero, copy_map) == ' ')
+                ) {
+                turn_right(&copy_hero);
+            }
+
+            if (next_val(&copy_hero, copy_map) != '1') {
+                step_forward(&copy_hero);
+
+                if (((copy_hero.pos_y_start == copy_hero.pos_y)
+                    && (copy_hero.pos_x_start == copy_hero.pos_x)))
+                    break;
+            }
+
+        }
+
+        system("clear");
+        map_print(copy_map);
+
+        printf("\n\npos_x: %d\n", copy_hero.pos_x);
+        printf("pos_y: %d\n", copy_hero.pos_y);
+        printf("direction: %c\n", copy_hero.direction);
+        printf("vector_x: %d\n", copy_hero.vector_x);
+        printf("vector_y: %d\n", copy_hero.vector_y);
+
+        if (next_val(&copy_hero, copy_map) == 'e')
+        {
+            printf("Error\n Map is not Valid\n");
+            check_process = 0;
+            break;
+        }
+
+        if ((copy_hero.pos_y_start == copy_hero.pos_y)
+            && (copy_hero.pos_x_start == copy_hero.pos_x) && cnt == 3)
+        {
+            printf("Map is Valid\n");
+            check_process = 0;
+            //break;
+        }
+    }
+
+
+    exit(0);
+
+    turn_right(&copy_hero);
+
+    exit(0);
+
+//    Справа стена {
+//      i = 0
+//        Пока Впереди стена {
+//            if (++i == 4)
+//              {return valid}
+//            Поворот налево
+//        }
+//        Шаг Вперед
+//    } else {
+//        Поворот направо
+//        Шаг вперед
+//
+//    }
+
 
     return (1);
 
@@ -98,7 +477,7 @@ int             main(int argc, char **argv)
 
 
     if (!create_map( argv[1])) {
-        perror("Error create_map");
+        printf("Error create_map");
     }
 
 //    printf("S isAllowed: %d\n", is_allowed_char('S'));
